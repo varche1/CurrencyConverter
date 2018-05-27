015c321e5c7603206cfcf88b57207d2cd09ddee2
Ivan Gureev <dev2@topnomer.ru>
2018-05-27 20:00:43 +0300

    LastConverted component

---
+++ app/components/Text/LastConverted.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+import styles from './styles';
+
+const LastConverted = ({
+  base, conversionRate, quote, date,
+}) => (
+  <Text style={styles.smallText}>
+    1 {base} = {conversionRate} {quote} as of {moment(date).format('MMMM D, YYYY')}
+  </Text>
+);
+
+LastConverted.propTypes = {
+  date: PropTypes.object,
+  base: PropTypes.string,
+  quote: PropTypes.string,
+  conversionRate: PropTypes.number,
+};
+
+export default LastConverted;
