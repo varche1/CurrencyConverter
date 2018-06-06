17389b9c9c19056d90a71ae00be1b95bfbbad0f6
Ivan Gureev <dev2@topnomer.ru>
2018-06-06 22:31:05 +0300

    Themes screen

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+
   $white: '#fff',
   $border: '#e2e2e2',
   $inputText: '#797979',
@@ -14,4 +18,4 @@ EStyleSheet.build({
   // $outline: 1,
 });
 
-export default () => <Options />;
+export default () => <Themes />;
