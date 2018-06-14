c1633ecaf4ce56974788cc2e3b457ac65f31d6cd
Ivan Gureev <dev2@topnomer.ru>
2018-06-14 22:29:04 +0300

    Curreny list to redux

--- app/reducers/currencies.js
+++ app/reducers/currencies.js
@@ -1,4 +1,9 @@
-import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY } from '../actions/currencies';
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY,
+} from '../actions/currencies';
 
 const initialState = {
   baseCurrency: 'USD',
@@ -46,6 +51,23 @@ const initialState = {
   },
 };
 
+const setConversions = (state, action) => {
+  let conversions = {
+    isFetching: true,
+    date: '',
+    rates: {},
+  };
+
+  if (state.conversions[action.currency]) {
+    conversions = state.conversions[action.currency];
+  }
+
+  return {
+    ...state.conversions,
+    [action.currency]: conversions,
+  };
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_CURRENCY_AMOUNT:
@@ -55,6 +77,18 @@ export default (state = initialState, action) => {
         ...state,
         baseCurrency: state.quoteCurrency,
         quoteCurrency: state.baseCurrency,
+      };
+    case CHANGE_BASE_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: action.currency,
+        conversions: setConversions(state, action),
+      };
+    case CHANGE_QUOTE_CURRENCY:
+      return {
+        ...state,
+        quoteCurrency: action.currency,
+        conversions: setConversions(state, action),
       };
     default:
       return state;
